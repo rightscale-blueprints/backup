cca1ed580d8e2e55da737e9761989265fdd6f671
Chris Fordham <chris@xhost.com.au>
2013-01-09 16:01:23 +1100

    Another minor metadata fix for backup cookbook from upstream via librarian fetch.

--- cookbooks/backup/metadata.rb
+++ cookbooks/backup/metadata.rb
@@ -7,100 +7,100 @@ version          "0.0.1"
 
 depends "aws"
 
-recipe "backup", "Sets up the backup RubyGem (plus configuration) and it's depenencies."
+recipe "backup", "Sets up the backup RubyGem (plus configuration) and it's dependencies."
 
 attribute "backup/user",
   :display_name => "Backup User",
   :description => "The system user for backups.",
   :required => "recommended",
   :default => "root",
-  :recipes => [ "backup" ]
+  :recipes => [ "backup::default" ]
 
 attribute "backup/name",
   :display_name => "Backup default name",
   :description => "The the default name for backups.",
   :required => "optional",
   :default => "server_backup",
-  :recipes => [ "backup" ]
+  :recipes => [ "backup::default" ]
   
 attribute "backup/description",
   :display_name => "Backup default description",
   :description => "The the default description for backups.",
   :required => "optional",
   :default => "a chef generated server backup",
-  :recipes => [ "backup" ]
+  :recipes => [ "backup::default" ]
 
 attribute "backup/database/username",
   :display_name => "Backup database user",
   :description => "The db user for database-related backups.",
   :required => "optional",
   :default => 'mysql',
-  :recipes => [ "backup" ]
+  :recipes => [ "backup::default" ]
 
 attribute "backup/database/password",
   :display_name => "Backup database password",
   :description => "The db pasword for database-related backups.",
   :required => "optional",
-  :recipes => [ "backup" ]
+  :recipes => [ "backup::default" ]
 
 attribute "backup/s3/bucket_region",
   :display_name => "Backup S3 bucket region",
   :description => "The default region for S3 buckets used with S3 related backups.",
   :required => "optional",
   :default => 'us-east-1',
-  :recipes => [ "backup" ]
+  :recipes => [ "backup::default" ]
   
 attribute "backup/s3/bucket_name",
   :display_name => "Backup S3 bucket name",
   :description => "The default S3 bucket name used with S3 related backups.",
   :required => "optional",
-  :recipes => [ "backup" ]
+  :recipes => [ "backup::default" ]
   
 attribute "backup/s3/keep",
   :display_name => "Backup S3 keep",
   :description => "The number of S3 backets to keep with S3 related backups.",
   :required => "recommended",
   :default => '10',
-  :recipes => [ "backup" ]
+  :recipes => [ "backup::default" ]
   
 attribute "backup/mail/on_success",
   :display_name => "Backup mail on success",
   :description => "Whether to email someone on success of a backup.",
   :required => "optional",
   :default => "true",
-  :recipes => [ "backup" ]
+  :recipes => [ "backup::default" ]
   
 attribute "backup/mail/on_success",
   :display_name => "Backup mail on failure",
   :description => "Whether to email someone on failure of a backup.",
   :required => "optional",
   :default => "true",
-  :recipes => [ "backup" ]  
+  :recipes => [ "backup::default" ]  
   
 attribute "backup/mail/from_address",
   :display_name => "Backup mail From: address",
   :description => "The email address mails should be spoofed from.",
   :required => "optional",
   :default => "backup@doesnotexist.com",
-  :recipes => [ "backup" ] 
+  :recipes => [ "backup::default" ] 
 
 attribute "backup/mail/from_address",
   :display_name => "Backup mail To: address",
   :description => "The email address mails should be sent to.",
   :required => "optional",
   :default => "backup@doesnotexist.com",
-  :recipes => [ "backup" ]
+  :recipes => [ "backup::default" ]
   
 attribute "aws/aws_access_key_id",
   :display_name => "AWS Access Key ID",
   :description => "The AWS access key ID to be used with EC2 related backups.",
   :required => "recommended",
   :default => "YOUR_ACCESS_KEY",
-  :recipes => [ "aws", "backup" ]
+  :recipes => [ "aws::default", "backup::default" ]
 
 attribute "aws/aws_secret_access_key",
   :display_name => "AWS Secret Access Key",
   :description => "The AWS secret access key to be used with EC2 related backups.",
   :required => "recommended",
   :default => "YOUR_SECRET_ACCESS_KEY",
-  :recipes => [ "aws", "backup" ]
+  :recipes => [ "aws::default", "backup::default" ]
