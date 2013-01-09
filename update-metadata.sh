e384cc38cb11fad5d30c87fd01b78bcd0d3f2375
Chris Fordham <chris@xhost.com.au>
2013-01-09 15:24:51 +1100

    Remove -e from shebang and use ; instead of && for .gitignore strips.

--- update-metadata.sh
+++ update-metadata.sh
@@ -1,12 +1,12 @@
-#!/bin/sh -e
+#!/bin/sh
 
 # generate metadata.json for all upstream opscode cookbooks (metadata.json should not exist in development repositories)
 knife cookbook metadata apache2 -o cookbooks/
 knife cookbook metadata aws -o cookbooks/
 knife cookbook metadata backup -o cookbooks/
 knife cookbook metadata build-essential -o cookbooks/
-knife cookbook metadata collectd -o cookbooks/ && rm cookbooks/collectd/.gitignore
-knife cookbook metadata collectd_plugins -o cookbooks/ && rm cookbooks/collectd_plugins/.gitignore
+knife cookbook metadata collectd -o cookbooks/; rm cookbooks/collectd/.gitignore
+knife cookbook metadata collectd_plugins -o cookbooks/; rm cookbooks/collectd_plugins/.gitignore
 knife cookbook metadata cron -o cookbooks/
 knife cookbook metadata ntp -o cookbooks/
 knife cookbook metadata postfix -o cookbooks/
